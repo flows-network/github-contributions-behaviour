ff2988434450a2c5232081edf9e88db0aef22cde
JYC0413 <juyichen0413@foxmail.com>
2023-09-06 02:11:31 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,21 +27,7 @@ export default function MyApp({Component, pageProps}: MyAppProps) {
 
     return (
         <>
-            <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-85B8CW1R8E"/>
-            <Script
-                id='google-analytics'
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `window.dataLayer = window.dataLayer || [];
-                             function gtag(){
-                             dataLayer.push(arguments);
-                             }
-                             gtag('js', new Date());
-                             gtag('config', 'G-85B8CW1R8E', {
-                             page_path: window.location.pathname,
-                             });`
-                }}
-            />
+            <script src="https://accounts.google.com/gsi/client" async defer/>
             <Head>
                 <title>Flows.network</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
