901531c8070edcc0167b03016e2129143517052a
JYC0413 <juyichen0413@foxmail.com>
2023-09-11 11:51:47 +0800

    fix

--- pages/api/authorized.js
+++ pages/api/authorized.js
@@ -1,3 +1,4 @@
+import querystring from 'querystring';
 //when user login with GitHub success, will redirect like https://github.com/login/oauth/authorize?client_id= {user's code} and to get this code
 export default async function (req, res) {
     if (req.query.code) {
@@ -8,16 +9,20 @@ export default async function (req, res) {
     </script>
   `);
 
-    } else if (req.body.credential) {
-        res.send(`
+    } else if (req.body) {
+        const params = querystring.parse(req.body);
+        if (params.credential) {
+            res.send(`
     <script>
-      window.opener.sign('${req.body.credential}',"google");
+      window.opener.sign('${params.credential}',"google");
       window.close();
     </script>
   `);
+        } else {
+            res.status(400).send('No code supplied');
+        }
     } else {
         res.status(400).send('No code supplied');
-        return;
     }
 
 //pass code and request which header(components) set in window
