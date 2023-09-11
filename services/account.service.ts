5cbbe2428283d6491b130322c36ba3d87e805186
JYC0413 <juyichen0413@foxmail.com>
2023-09-11 12:09:54 +0800

    fix

--- services/account.service.ts
+++ services/account.service.ts
@@ -1,5 +1,6 @@
 import {BehaviorSubject} from 'rxjs';
 const userSubject = new BehaviorSubject(null);//user information we got from GitHub
+import {fetchWrapper} from '../utils';
 export default {
     sign,
     signByGoogle,
@@ -17,13 +18,7 @@ async function sign(code: string): Promise<boolean> {
 }
 
 async function signByGoogle(code: string): Promise<boolean> {
-    let result = await fetch(`/api/signByGoogle`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json' // 根据你的需求设置适当的 Content-Type
-        },
-        body: JSON.stringify({code})
-    });
+    let result = await fetchWrapper.post(`/api/signByGoogle`,code);
     if (result.status === 200) {
         userSubject.next(await result.json());
         return true;
